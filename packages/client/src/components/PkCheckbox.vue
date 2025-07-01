030d2211c02359855bea3448e92f1713f12587cc
Mariia Kondrateva <thesmallestgnome@gmail.com>
2025-07-02 01:31:44 +1200

    add css styles to checkboxes

--- packages/client/src/components/PkCheckbox.vue
+++ packages/client/src/components/PkCheckbox.vue
@@ -41,11 +41,45 @@ const handleChange = (event: Event) => {
 }
 
 .pk-checkbox input[type="checkbox"] {
+  /* Add if not using autoprefixer */
+  -webkit-appearance: none;
+  appearance: none;
+  /* For iOS < 15 to remove gradient background */
+  background-color: #fff;
+  /* Not removed via appearance */
+  margin: 0;
+
+  /* New styles */
   width: 1rem;
   height: 1rem;
   cursor: pointer;
-  margin: auto var(--padding-sixth);
+  font: inherit;
+  color: currentColor;
+  border: var(--border-input-idle);
+  border-radius: var(--border-radius-extra-small-box);
+
+  /* This is the quickest way to align the :before to the horizontal and vertical center of our custom control */
+  display: grid;
+  place-content: center;
 }
+
+.pk-checkbox input[type="checkbox"]:checked{
+  border: var(--border-input-active);
+}
+
+.pk-checkbox input[type="checkbox"]::before {
+  content: "";
+  width: 0.65rem;
+  height: 0.65rem;
+  transform: scale(0);
+  transition: 120ms transform ease-in-out;
+  box-shadow: inset 1em 1em var(--form-control-color);
+}
+
+.pk-checkbox input[type="checkbox"]:checked::before {
+  transform: scale(1);
+}
+
 
 .pk-checkbox input[type="checkbox"]:disabled {
   cursor: not-allowed;
