1762855638e2b977e1da68e39347dd6ade44c821
Andrew Kondratev <andrei.kondratev@unimarket.com>
2025-07-12 00:03:48 +1200

    hooks

--- packages/server-js/.gsloth.config.mjs
+++ packages/server-js/.gsloth.config.mjs
@@ -1,9 +1,30 @@
 import {z} from "zod";
 import {HumanMessage} from '@langchain/core/messages';
+import {GthLangChainAgent} from "gaunt-sloth-assistant/core/GthLangChainAgent";
+
+/**
+ * @typedef {import('gaunt-sloth-assistant/config').GthConfig} GthConfig
+ * @typedef {import('@langchain/core/runnables').RunnableConfig} RunnableConfig
+ */
+
+
+/**
+ * This is a cli util, so one run, one session
+ */
+const sessionState = {
+  /**
+   * @type RunnableConfig
+   */
+  runConfig: undefined
+};
 
 export async function configure() {
   const {ChatVertexAI} = await import('@langchain/google-vertexai');
-  return {
+  // noinspection UnnecessaryLocalVariableJS
+  /**
+   * @type GthConfig
+   */
+  const config = {
     llm: new ChatVertexAI({
       model: 'gemini-2.5-pro',
     }),
@@ -13,37 +34,42 @@ export async function configure() {
         'url': 'http://localhost:3002/mcp'
       }
     },
-    mcpInitHandler: (
-      config,
-      invocation,
-      multiClient
-    ) => {
-      console.log('mcpInitHandler', multiClient);
-
-      /**
-       * FIXME somehow when doing multiClient.getClient('pukeko-ui') too early we end up with two instances of the client.
-       * FIXME We need to figure out something better than the timeout
-       */
-      setTimeout(() => {
-        multiClient.getClient('pukeko-ui').then((client) => {
-          client.setRequestHandler(z.object({
-            method: z.literal('message'),
-            // xxx it seems like this listener function can't be async
-          }), (props, extra) => {
-            // FIXME ideally this should know about the thread ID
-            console.log('request handler', props, extra);
-            invocation.invoke([new HumanMessage('user cancelled the form')]).then((r) => {
-              console.log('invoked');
+    hooks: {
+      beforeProcessMessages: (runner, messages, runConfig) => {
+        // Safe run config fom first message
+        if (!sessionState.runConfig) {
+          sessionState.runConfig = runConfig;
+        }
+      },
+      afterAgentInit: (runner) => {
+        const agent = runner.getAgent();
+        if (agent instanceof GthLangChainAgent) {
+          agent.getMCPClient().getClient('pukeko-ui').then((client) => {
+            console.log('beforeAgentInit acquired pukeko-ui client');
+            client.setRequestHandler(z.object({
+              method: z.literal('message'),
+              // xxx it seems like this listener function can't be async
+            }), (props, extra) => {
+              console.log('request handler', props, extra);
+              // FIXME this explodes without runnable
+              if (sessionState.runConfig) {
+                runner.processMessages([new HumanMessage('user cancelled the form')], sessionState.runConfig).then((r) => {
+                  console.log('invoked');
+                });
+              } else {
+                console.log('no run config');
+              }
+              return {
+                ack: true,
+              };
             });
-            return {
-              ack: true,
-            };
+            console.log('client', client);
           });
-          console.log('client', client);
-        });
-      }, 5000);
+        }
+      }
     }
   };
+  return config;
 }
 
 /**
